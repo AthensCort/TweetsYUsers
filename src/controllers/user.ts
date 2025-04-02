f38555171ba554ff080905eb7a5ad1144344d324
AthensCort <athens.cort@gmail.com>
2025-04-02 15:40:03 -0600

    User database/ controller

---
+++ src/controllers/user.ts
@@ -0,0 +1,18 @@
+
+import UserService from "../db/user";
+
+class UserController{
+
+    //modificando tmb la parte de use service pq el type of
+    service: typeof UserService; 
+    constructor(service: typeof UserService){
+        this.service = service;
+    }
+
+    async getAllUsers() {
+        const users = this.service.getEverythingUsers(); 
+        return users.filter(user => user % 2 === 0); // Filtrar solo los pares
+    }
+}
+
+export default UserController;
